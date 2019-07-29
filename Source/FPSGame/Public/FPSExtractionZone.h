533cef3a8869a403a5f063a05a3d0cbb95d4b38b
Forest Guhareza <arza.3d@gmail.com>
2019-07-29 10:02:10 +0700

    update master, short code

--- Source/FPSGame/Public/FPSExtractionZone.h
+++ Source/FPSGame/Public/FPSExtractionZone.h
@@ -12,8 +12,8 @@ UCLASS()
 class FPSGAME_API AFPSExtractionZone : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 
 	AFPSExtractionZone();
 
@@ -23,6 +23,7 @@ protected:
 	UBoxComponent* OverlapComp;
 
 	UFUNCTION()
-	void HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+	void HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
 
 };
