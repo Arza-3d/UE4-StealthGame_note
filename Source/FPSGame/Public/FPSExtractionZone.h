9fd4289335ca498b0ace87d2267a32251dfcd10e
Forest Guhareza <arza.3d@gmail.com>
2019-07-29 10:02:41 +0700

    short code

--- Source/FPSGame/Public/FPSExtractionZone.h
+++ Source/FPSGame/Public/FPSExtractionZone.h
@@ -23,6 +23,7 @@ protected:
 	UBoxComponent* OverlapComp;
 
 	UFUNCTION()
-	void HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+	void HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
 	// R3- the above function is taken from the BeginOverlapped delegate from primitive component?
 };
