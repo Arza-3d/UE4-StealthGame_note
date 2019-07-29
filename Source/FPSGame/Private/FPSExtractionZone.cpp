3ebd9d83e3e3d8aecbe27023fcb182f603457916
Forest Guhareza <arza.3d@gmail.com>
2019-07-29 13:34:11 +0700

    delete comment

--- Source/FPSGame/Private/FPSExtractionZone.cpp
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -7,7 +7,6 @@
 #include "FPSCharacter.h"
 #include "FPSGameMode.h"
 
-// Sets default values
 AFPSExtractionZone::AFPSExtractionZone()
 {
 
@@ -27,7 +26,7 @@ AFPSExtractionZone::AFPSExtractionZone()
 	DecalComp->SetupAttachment(RootComponent);
 }
 
-void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, 
+void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor,
 	UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
 {
 	UE_LOG(LogTemp, Log, TEXT("Overlapped extraction zone!"));
@@ -38,7 +37,7 @@ void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent * OverlappedComponent
 		AFPSGameMode* GM = Cast<AFPSGameMode>(GetWorld()->GetAuthGameMode());
 		if (GM)
 		{
-			GM->CompleteMission(MyPawn); // this function will disable the controller on this character
+			GM->CompleteMission(MyPawn);
 		}
 	}
 }
