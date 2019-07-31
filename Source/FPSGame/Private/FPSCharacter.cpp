a64819589470ea3bc075492d42bf7233f045ae7d
Forest Guhareza <arza.3d@gmail.com>
2019-07-31 17:38:38 +0700

    commented

--- Source/FPSGame/Private/FPSCharacter.cpp
+++ Source/FPSGame/Private/FPSCharacter.cpp
@@ -1,5 +1,3 @@
-// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
-
 #include "FPSCharacter.h"
 #include "FPSProjectile.h"
 #include "Animation/AnimInstance.h"
@@ -11,20 +9,17 @@
 
 AFPSCharacter::AFPSCharacter()
 {
-	// Create a CameraComponent	
 	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
 	CameraComponent->SetupAttachment(GetCapsuleComponent());
-	CameraComponent->RelativeLocation = FVector(0, 0, BaseEyeHeight); // Position the camera
+	CameraComponent->RelativeLocation = FVector(0, 0, BaseEyeHeight);
 	CameraComponent->bUsePawnControlRotation = true;
 
-	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
 	Mesh1PComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh"));
 	Mesh1PComponent->SetupAttachment(CameraComponent);
 	Mesh1PComponent->CastShadow = false;
 	Mesh1PComponent->RelativeRotation = FRotator(2.0f, -15.0f, 5.0f);
 	Mesh1PComponent->RelativeLocation = FVector(0, 0, -160.0f);
 
-	// Create a gun mesh component
 	GunMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FP_Gun"));
 	GunMeshComponent->CastShadow = false;
 	GunMeshComponent->SetupAttachment(Mesh1PComponent, "GripPoint");
@@ -35,7 +30,6 @@ AFPSCharacter::AFPSCharacter()
 
 void AFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	// set up gameplay key bindings
 	check(PlayerInputComponent);
 
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
@@ -48,6 +42,8 @@ void AFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompon
 	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 }
 
+////////////////////////////////////////////////////////////1a
+// NEW: Update with remote view pitch
 void AFPSCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
@@ -56,27 +52,24 @@ void AFPSCharacter::Tick(float DeltaTime)
 	{
 		FRotator NewRot = CameraComponent->RelativeRotation;
 		NewRot.Pitch = RemoteViewPitch * 360.0f / 255.0f;
+		// the view pitch was 8bit
 
 		CameraComponent->SetRelativeRotation(NewRot);
 	}
-	
 }
-
+////////////////////////////////////////////////////////////1z
 
 void AFPSCharacter::Fire()
 {
 	ServerFire();
 
-	// try and play the sound if specified
 	if (FireSound)
 	{
 		UGameplayStatics::PlaySoundAtLocation(this, FireSound, GetActorLocation());
 	}
 
-	// try and play a firing animation if specified
 	if (FireAnimation)
 	{
-		// Get the animation object for the arms mesh
 		UAnimInstance* AnimInstance = Mesh1PComponent->GetAnimInstance();
 		if (AnimInstance)
 		{
@@ -88,18 +81,15 @@ void AFPSCharacter::Fire()
 void AFPSCharacter::ServerFire_Implementation()
 {
 
-	// try and fire a projectile
 	if (ProjectileClass)
 	{
 		FVector MuzzleLocation = GunMeshComponent->GetSocketLocation("Muzzle");
 		FRotator MuzzleRotation = GunMeshComponent->GetSocketRotation("Muzzle");
 
-		//Set Spawn Collision Handling Override
 		FActorSpawnParameters ActorSpawnParams;
 		ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
 		ActorSpawnParams.Instigator = this;
 
-		// spawn the projectile at the muzzle
 		GetWorld()->SpawnActor<AFPSProjectile>(ProjectileClass, MuzzleLocation, MuzzleRotation, ActorSpawnParams);
 	}
 
@@ -107,7 +97,7 @@ void AFPSCharacter::ServerFire_Implementation()
 
 bool AFPSCharacter::ServerFire_Validate()
 {
-	return true;//server request if  client return false it probably mean disconnect, you can do check later
+	return true;
 }
 
 
@@ -115,7 +105,6 @@ void AFPSCharacter::MoveForward(float Value)
 {
 	if (Value != 0.0f)
 	{
-		// add movement in that direction
 		AddMovementInput(GetActorForwardVector(), Value);
 	}
 }
@@ -125,7 +114,6 @@ void AFPSCharacter::MoveRight(float Value)
 {
 	if (Value != 0.0f)
 	{
-		// add movement in that direction
 		AddMovementInput(GetActorRightVector(), Value);
 	}
 }
--- Source/FPSGame/Public/FPSCharacter.h
+++ Source/FPSGame/Public/FPSCharacter.h
@@ -1,5 +1,3 @@
-// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -22,15 +20,12 @@ class AFPSCharacter : public ACharacter
 
 protected:
 
-	/** Pawn mesh: 1st person view  */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Mesh")
 	USkeletalMeshComponent* Mesh1PComponent;
 
-	/** Gun mesh: 1st person view (seen only by self) */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Mesh")
 	USkeletalMeshComponent* GunMeshComponent;
 
-	/** First person camera */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
 	UCameraComponent* CameraComponent;
 
@@ -40,44 +35,39 @@ protected:
 public:
 	AFPSCharacter();
 
-	/** Projectile class to spawn */
 	UPROPERTY(EditDefaultsOnly, Category="Projectile")
 	TSubclassOf<AFPSProjectile> ProjectileClass;
 
-	/** Sound to play each time we fire */
 	UPROPERTY(EditDefaultsOnly, Category="Gameplay")
 	USoundBase* FireSound;
 
-	/** AnimMontage to play each time we fire */
 	UPROPERTY(EditDefaultsOnly, Category = "Gameplay")
 	UAnimSequence* FireAnimation;
-	
+
 	UPROPERTY(BlueprintReadOnly, Category = "Gameplay")
 	bool bIsCarryingObjective;
 
 protected:
-	
-	/** Fires a projectile. */
+
 	void Fire();
 
 	UFUNCTION(Server, Reliable, WithValidation)
 	void ServerFire();
 
-	/** Handles moving forward/backward */
 	void MoveForward(float Val);
 
-	/** Handles strafing movement, left and right */
 	void MoveRight(float Val);
 
 	virtual void SetupPlayerInputComponent(UInputComponent* InputComponent) override;
 
 public:
-	/** Returns Mesh1P subobject **/
+
 	USkeletalMeshComponent* GetMesh1P() const { return Mesh1PComponent; }
 
-	/** Returns FirstPersonCameraComponent subobject **/
 	UCameraComponent* GetFirstPersonCameraComponent() const { return CameraComponent; }
 
+	///////////////////////////////////////////////1a
+	//NEW: update pitch
 	virtual void Tick(float DeltaTime) override;
+	///////////////////////////////////////////////1z
 };
-
