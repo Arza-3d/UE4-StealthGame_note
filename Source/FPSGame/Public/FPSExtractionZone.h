6c9ea4f49cd49b07e46151ad2c8f3b513a5f5e72
Forest Guhareza <arza.3d@gmail.com>
2019-07-31 20:51:06 +0700

    update comment

--- Source/FPSGame/Public/FPSExtractionZone.h
+++ Source/FPSGame/Public/FPSExtractionZone.h
@@ -21,7 +21,7 @@ protected:
 	UBoxComponent* OverlapComp;
 
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////Ia
-	// NOTE: alternative function to NotifyActorBeginOverlap
+	// NOTE: alternative function to NotifyActorBeginOverlap using delegate from PrimitiveComponent.h
 	UFUNCTION()
 	void HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
