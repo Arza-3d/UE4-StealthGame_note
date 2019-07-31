9dc33c652573aac19758dc43e80e0ff3af6e785a
Forest Guhareza <arza.3d@gmail.com>
2019-07-31 22:45:10 +0700

    update

--- Source/FPSGame/Private/FPSAIGuard.cpp
+++ Source/FPSGame/Private/FPSAIGuard.cpp
@@ -1,50 +1,35 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "FPSAIGuard.h"
 #include "Perception/PawnSensingComponent.h"
 #include "DrawDebugHelpers.h"
 
-// Sets default values
 AFPSAIGuard::AFPSAIGuard()
 {
-
 	PrimaryActorTick.bCanEverTick = true;
 
 	PawnSensingComp = CreateDefaultSubobject<UPawnSensingComponent>(TEXT("PawnSensingComp"));
 
 	PawnSensingComp->OnSeePawn.AddDynamic(this, &AFPSAIGuard::OnPawnSeen);
+
+	//////////////////////////////////////////////////////////////////////////////1a
+	// NEW: bind function to OnHearNoise delegate
 	PawnSensingComp->OnHearNoise.AddDynamic(this, &AFPSAIGuard::OnNoiseHeard);
-
+	//////////////////////////////////////////////////////////////////////////////1z
 }
-
-void AFPSAIGuard::BeginPlay()
-{
-	Super::BeginPlay();
-
-}
-
 
 void AFPSAIGuard::OnPawnSeen(APawn* SeenPawn)
 {
-
 	if (SeenPawn == nullptr)
 	{
 		return;
 	}
 
 	DrawDebugSphere(GetWorld(), SeenPawn->GetActorLocation(), 32.0f, 12, FColor::Red, false, 10.0f);
-
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////2a
+// NEW: function to bind
 void AFPSAIGuard::OnNoiseHeard(APawn* NoiseInstigator, const FVector & Location, float Volume)
 {
 	DrawDebugSphere(GetWorld(), Location, 32.0f, 12, FColor::Green, false, 10.0f);
 }
-
-
-void AFPSAIGuard::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
+/////////////////////////////////////////////////////////////////////////////////////////////////2z
--- Source/FPSGame/Public/FPSAIGuard.h
+++ Source/FPSGame/Public/FPSAIGuard.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -19,19 +17,16 @@ public:
 
 protected:
 
-	virtual void BeginPlay() override;
-
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UPawnSensingComponent* PawnSensingComp;
 
 	UFUNCTION()
 	void OnPawnSeen(APawn* SeenPawn);
 
+	/////////////////////////////////////////////////////////////////////////////////////1a
+	// NEW: function to binds
 	UFUNCTION()
 	void OnNoiseHeard(APawn* NoiseInstigator, const FVector& Location, float Volume);
-
-public:
-
-	virtual void Tick(float DeltaTime) override;
+	/////////////////////////////////////////////////////////////////////////////////////1z
 
 };
