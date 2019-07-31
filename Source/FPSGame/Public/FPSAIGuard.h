da3431d47ea4459cf97603f2930c9b9c8a5f3648
Forest Guhareza <arza.3d@gmail.com>
2019-07-31 22:04:52 +0700

    make the guardAI

--- Source/FPSGame/Public/FPSAIGuard.h
+++ Source/FPSGame/Public/FPSAIGuard.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -18,14 +16,7 @@ public:
 	AFPSAIGuard();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UPawnSensingComponent* PawnSensingComp;
-
-public:	
-
-	virtual void Tick(float DeltaTime) override;
-
 };
