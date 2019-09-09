668b5fafdcb15f51962bb727d5529304013ac706
Forest Guhareza <arza.3d@gmail.com>
2019-09-10 06:16:47 +0700

    blackhole

--- Source/FPSGame/Private/FPSBlackHole.cpp
+++ Source/FPSGame/Private/FPSBlackHole.cpp
@@ -39,7 +39,7 @@ void AFPSBlackHole::Tick(float DeltaTime)
 
 		if (primComp && primComp->IsSimulatingPhysics())
 		{
-			primComp->AddRadialForce(GetActorLocation(), sphereRadius, -3000.0f, ERadialImpulseFalloff::RIF_Constant,	true);
+			primComp->AddRadialForce(GetActorLocation(), sphereRadius, -3000.0f, ERadialImpulseFalloff::RIF_Constant, true);
 		}
 	}
 
